ef351154c5e310d1a7f27ebde287635764e00736
Ousmane Diallo <info.ouzdiallo@gmail.com>
2021-06-22 12:02:16 +0000

    Creation de l'entité Announce

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,22 +6,24 @@ use App\Entity\Announce;
 use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+
         for ($i = 0; $i < 20; $i++) {
             $announce = new Announce();
-            $announce->setTitle("chambre n° $i");
-            $announce->setSlug("chambre-$i");
-            $announce->setDescription("Je vous loue ma chambre avec une salle de bain très neuve...");
-            $announce->setPrice(mt_rand(30000, 60000));
-            $announce->setAddress("quartier $i");
-            $announce->setCoverImage("https://via.placeholder.com/500x300");
-            $announce->setRooms(mt_rand(0, 5));
-            $announce->setIsAvailable(mt_rand(0, 1));
-            $announce->setCreatedAt(new DateTime());
+            $announce->setTitle("chambre n° $i")
+                ->setSlug("chambre-$i")
+                ->setDescription("Je vous loue ma chambre avec salle bain neuve...")
+                ->setPrice(mt_rand(30000, 60000))
+                ->setAddress("quartier $i")
+                ->setCoverImage("https://via.placeholder.com/500x300")
+                ->setRooms(mt_rand(0, 5))
+                ->setIsAvailable(mt_rand(0, 1))
+                ->setCreatedAt(new DateTime());
 
             $manager->persist($announce);  // Permet à Doctrine d'enregistrer l'annonce dans la DB
         }
